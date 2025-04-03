714ef8a4caa612083a27b5b2a909031fe60054e9
Daniyal <133042543+D1207-D@users.noreply.github.com>
2025-04-03 13:31:00 -0400

    Fix federated credential JSON files and add resource group output

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "app" {
   name     = "jose0337-a12-rg"
   location = "canadacentral"
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.app.name
+}
